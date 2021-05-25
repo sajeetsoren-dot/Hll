afa79f2500519dec688d38e3b9e1d0ca278dbf16
Sajeet <78308132+sajeetsoren-dot@users.noreply.github.com>
2021-05-26 05:10:56 +0530

    Add files via upload

---
+++ hell1_cs_consloe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hell1_cs_consloe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("i am very happy that i am doing this");
+        }
+    }
+}
